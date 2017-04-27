0813bdf3877fdc67aa502240747da625ccf15616
AndrejBegovic <a.begovic@hotmail.co.uk>
2017-04-27 15:56:28 +0200

    Update program.cpp

--- GameOfLife/GameOfLife/program.cpp
+++ GameOfLife/GameOfLife/program.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
 #include "game_of_life.h"
 using namespace std;
 
 int main() {
+
+	srand(time(nullptr));
+
 	game_of_life the_game;
 
 	bool dalje;
-	do {
-		the_game.iscrtaj();
+
+	cout << "Dalje (1/0): ";
+	cin >> dalje;
+
+	while (dalje) {
+
+		system("cls");
+
 		the_game.sljedeca_generacija();
 
 		cout << "Dalje (1/0): ";
 		cin >> dalje;
-	} while (dalje);
 
+	}
+	
 	return 0;
 }
